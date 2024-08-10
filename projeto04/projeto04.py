832c936552155727d7e8f192a1b8f5ef9affd045
Edson Sousa <edson3711.es@gmail.com>
2024-08-10 15:22:51 -0300

    projeto 04

--- projeto04/projeto04.py
+++ projeto04/projeto04.py
@@ -1,9 +1,14 @@
 import cv2
+from cvzone.HandTrackingModule import HandDetector
 
 webcam = cv2.VideoCapture(0)
+rastrador = HandDetector(detectionCon=0.8, maxHands = 2)
 
 while True:
     sucesso,imagem = webcam.read()
+    coordenadas,imagem_maos = rastrador.findHands(imagem)
+    print(coordenadas)
+
     cv2.imshow("Projeto 4 - IA",imagem)
 
     if cv2.waitKey(1) != -1:
